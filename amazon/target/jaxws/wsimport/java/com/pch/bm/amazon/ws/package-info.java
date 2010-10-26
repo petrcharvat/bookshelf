f2b7686a35e11b5216e5c47a5bd5274886a28678
Petr Charvat <petr.charvat@gmail.com>
2010-10-26 14:10:41 +0200

    remove targets

--- amazon/target/jaxws/wsimport/java/com/pch/bm/amazon/ws/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://webservices.amazon.com/AWSECommerceService/2009-11-01", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.pch.bm.amazon.ws;
